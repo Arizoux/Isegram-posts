1ab34481be5f06254cde859568a0062b093c87bb
Felix Hasel <f.hasel@campus.tu-berlin.de>
2025-02-02 21:13:01 +0100

    test

--- postsMS/settings.py
+++ postsMS/settings.py
@@ -43,6 +43,14 @@ INSTALLED_APPS = [
     'corsheaders',
 ]
 
+"""
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.simplejwt.authentication.JWTAuthentication',
+    ),
+}
+"""
+
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',  
     'django.middleware.security.SecurityMiddleware',
